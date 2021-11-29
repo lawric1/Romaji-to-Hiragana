e1cd7ee2d9e44e62c4136d8943aafd6c790b358d
lawric1 <67882089+lawric1@users.noreply.github.com>
2021-11-29 11:12:44 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var db = dict;
+
+window.onload = () => {
+    // var str = "arigatou"
+    var inputArea = document.getElementById("input");
+    var outputArea = document.getElementById("output");
+    var re = new RegExp(Object.keys(db).join("|"),"gi");
+
+    
+
+    inputArea.addEventListener('input', ()=>{
+        let string = input.value;
+
+        var result = string.replace(re, function(matched){
+            return db[matched];
+        });
+        
+        outputArea.value = result;
+        
+    }, false);
+
+}
